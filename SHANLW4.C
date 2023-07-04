12fd52e69618b7b5aecce9942d88394c01709465
jishandankhara <136707738+jishandankhara@users.noreply.github.com>
2023-07-04 16:50:26 +0530

    Add files via upload

---
+++ SHANLW4.C
@@ -0,0 +1,19 @@
+/*return
+aaaaa
+bbbbb
+ccccc
+ddddd
+eeeee*/
+
+#include<stdio.h>
+#include<conio.h>
+void main(){
+char i,j;
+clrscr();
+
+for(i='a';i<='e';i++){
+     for(j='a';j<='e';j++){printf("%c",i);  }
+     printf("\n");
+}
+
+getch();}
---
+++ SHANLW5.C
@@ -0,0 +1,18 @@
+/*return
+abcde
+abcde
+abcde
+abcde
+abcde*/
+#include<stdio.h>
+#include<conio.h>
+void main(){
+char i,j;
+clrscr();
+
+for(i='a';i<='e';i++){
+     for(j='a';j<='e';j++){printf("%c",j);  }
+     printf("\n");
+}
+
+getch();}
