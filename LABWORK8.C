bc8018d73ebc4e7998f01d80434daac3784aaa64
jishandankhara <136707738+jishandankhara@users.noreply.github.com>
2023-07-05 17:17:54 +0530

    LABWORK8.C

--- LABWORK8.C
+++ LABWORK8.C
@@ -1,17 +1,17 @@
 //7.2 Q8 write a program to print the below pattern using nested for loop.
 /* return
-12345
-2345
-345
-45
-5 */
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15*/
 #include<stdio.h>
 #include<conio.h>
 void main(){
-int i,j;
+int j,i,s=0;
 clrscr();
 for(i=1;i<=5;i++){
-   for(j=i;j<=5;j++){
-   printf("%d",j);}printf("\n");}
+   for(j=1;j<=i;j++){
+   printf("%d  ",s++);}printf("\n");}
 
 getch();}
